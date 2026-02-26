688b712385b0fed0a2d0fdde384bf53941608cac
Uday Surya P <udaysuryap.workspace@gmail.com>
2026-02-26 12:14:18 +0530

    Hitest choudary udemy task

---
+++ playingWithVariables.js
@@ -0,0 +1,50 @@
+/*
+1. Perform the following mathematical operations on the provided variables `a` and `b`
+   Add, Subtract, Multiply, Divide, Increment, Decrement, Remainder
+*/
+
+// **** DO NOT CHANGE the values *****
+const a = 18;
+const b = 24;
+
+// Add both values
+function add() {
+    return a + b;
+}
+// console.log(add()); // 42
+
+// Subtract smaller value from larger one
+function subtract() {
+    return b - a;
+}
+// console.log(subtract()); // 6
+
+// Multiply both values
+function multiply() {
+    return a * b;
+}
+// console.log(multiply()); // 432
+
+// Divide larger value by smaller
+function divide() {
+    return b / a;
+}
+// console.log(divide()); // 1.3333...
+
+// Increase value of a by 1
+function increment() {
+    return a + 1;
+}
+// console.log(increment()); // 19
+
+// Decrease value of b by 1
+function decrement() {
+    return b - 1;
+}
+// console.log(decrement()); // 23
+
+// Divide larger value by smaller to find the remainder
+function remainder() {
+    return b % a;
+}
+// console.log(remainder()); // 6
