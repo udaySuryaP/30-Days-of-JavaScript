189a36d954950e4b5927548d386bd3d058f3a70d
Uday Surya P <udaysuryap.workspace@gmail.com>
2026-02-27 10:35:36 +0530

    Hitesh Choudary Class Excercises

---
+++ nestedFunctionsAndContext.js
@@ -0,0 +1,27 @@
+/* 
+Task 1: Using `this` in Objects
+Create an object person with a method introduce() that uses `this`
+additionally add properties of name & age that will result in 
+Hi, my name is Hitesh and I am 19.5 years old on calling introduce()
+*/
+const person = {
+    name: "Hitesh",
+    age: 19.5,
+    introduce() {
+        return `Hi, my name is ${this.name} and I am ${this.age} years old`;
+    }
+};
+// console.log(person.introduce()); // Hi, my name is Hitesh and I am 19.5 years old
+
+/* 
+Task 2: Function within a function
+Write a function `outer()` that contains another function `inner()` and 
+returns a value of 'Inner function called' on calling outer()
+*/
+function outer() {
+    function inner() {
+        return "Inner function called";
+    }
+    return inner();
+}
+// console.log(outer()); // "Inner function called"
